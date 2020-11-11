ce53cd48d0b05ae357d1f9e9bbc16bd81a8b0e35
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-11 14:42:38 +0100

    Adding Facebook API to the project

--- ShareCaring/app/src/main/java/com/example/sharecaring/MainActivity.java
+++ ShareCaring/app/src/main/java/com/example/sharecaring/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sharecaring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 
 public class MainActivity extends AppCompatActivity {
 
