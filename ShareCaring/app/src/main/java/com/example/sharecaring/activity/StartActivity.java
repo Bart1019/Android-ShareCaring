63a9da3c53afb1a7c57c50c754513e958fa7cce2
Karolina Maciejewska <kmaciejewska14@gmail.com>
2020-11-29 14:09:02 +0100

    Added privacy policy for fb/google

--- ShareCaring/app/src/main/java/com/example/sharecaring/activity/StartActivity.java
+++ ShareCaring/app/src/main/java/com/example/sharecaring/activity/StartActivity.java
@@ -1,29 +1,78 @@
 package com.example.sharecaring.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sharecaring.R;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class StartActivity extends AppCompatActivity {
+    private static final String TAG = StartActivity.class.getName();
     private Dialog optionsDialog;
     private ImageView closePopUp;
     private Button optionsBtn, logInEmail, logIn;
+    private CallbackManager mCallbackManager;
+    private LoginButton loginButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        //initialize Facebook SDK
+        FacebookSdk.sdkInitialize(StartActivity.this);
+
+        //initialize Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        loginButton = findViewById(R.id.fbLogBtn);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
 
         optionsDialog = new Dialog(this);
 
@@ -35,6 +84,40 @@ public class StartActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void handleFacebookAccessToken(AccessToken token) { //called when the user gives the permission to access their data
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(StartActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(getApplicationContext(), InformationActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, getResources().getString(R.string.toast_fb_firebase), Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     private void showPopUp() {
         optionsDialog.setContentView(R.layout.popup);
