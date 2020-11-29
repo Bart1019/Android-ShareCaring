696715b582a048cbfa25555423dae2481ac7ae11
Wojciech <65348511+wojtas112@users.noreply.github.com>
2020-11-29 17:08:20 +0100

    adding google maps

--- ShareCaring/app/src/main/java/com/example/sharecaring/activity/InformationActivity.java
+++ ShareCaring/app/src/main/java/com/example/sharecaring/activity/InformationActivity.java
@@ -2,15 +2,34 @@ package com.example.sharecaring.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.sharecaring.R;
+import com.facebook.appevents.suggestedevents.ViewOnClickListener;
 
-public class InformationActivity extends AppCompatActivity {
+public class InformationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button gotIt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        gotIt = (Button)findViewById(R.id.btnGotIt);
+        gotIt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btnGotIt:
+                startActivity(new Intent(this, MapsActivity.class));
+                break;
+
+        }
     }
 }
