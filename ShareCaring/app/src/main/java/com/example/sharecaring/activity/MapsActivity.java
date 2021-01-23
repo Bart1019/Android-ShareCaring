18529e1042590972db247ea9159a748acdcf7526
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-01-23 17:29:32 +0100

    changing app structure to use fragments

--- ShareCaring/app/src/main/java/com/example/sharecaring/activity/MapsActivity.java
+++ ShareCaring/app/src/main/java/com/example/sharecaring/activity/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.sharecaring.activity;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
@@ -20,6 +21,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.sharecaring.R;
+import com.example.sharecaring.fragment.MapFragment;
+import com.example.sharecaring.fragment.NotificationsFragment;
+import com.example.sharecaring.fragment.OffersFragment;
+import com.example.sharecaring.fragment.ProfileFragment;
 import com.example.sharecaring.model.IntentOpener;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -44,16 +49,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
-
-    private static final String TAG = "MapsActivity";
-    private GoogleMap mMap;
-    public static final int FINE_LOCATION_ACCESS_REQUEST_CODE = 10001;
-    private FusedLocationProviderClient mFusedLocationClient;
-    DatabaseReference ref;
-    Geocoder geocoder;
-    List<Address> addressList = new ArrayList<>();
-    LatLng addressLatLng;
+public class MapsActivity extends FragmentActivity implements View.OnClickListener {
 
     BottomNavigationView bottomNavigationView;
     FloatingActionButton fabBtn;
@@ -67,13 +63,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
         fabBtn = findViewById(R.id.fab);
         fabBtn.setOnClickListener(this);
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
+        bottomNavigationView.setSelectedItemId(R.id.map);
     }
 
     @Override
@@ -87,113 +77,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
                     switch (item.getItemId()) {
                         case R.id.map:
-                            IntentOpener.openIntent(MapsActivity.this, MapsActivity.class);
+                            selectedFragment = new MapFragment();
                             break;
                         case R.id.profile:
-                            IntentOpener.openIntent(MapsActivity.this, ProfileActivity.class);
+                            selectedFragment = new ProfileFragment();
                             break;
                         case R.id.chat:
-                            IntentOpener.openIntent(MapsActivity.this, OfferList.class);
+                            selectedFragment = new OffersFragment();
                             break;
                         case R.id.notifications:
-                            IntentOpener.openIntent(MapsActivity.this, NotificationActivity.class);
+                            selectedFragment = new NotificationsFragment();
                             break;
                     }
-
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
                     return false;
                 }
             };
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        enableUserLocation();
-        getCurrentLocation();
-        //geocoder = new Geocoder(MapsActivity.this);
-        getAddresses();
-    }
-
-    private void getAddresses() {
-        /*ref = FirebaseDatabase.getInstance().getReference("Offers");
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot userId : snapshot.getChildren()) {
-                    for(DataSnapshot offerId : userId.getChildren()) {
-                        String addressFromDb = offerId.child("address").getValue().toString();
-                        try {
-                            addressList = geocoder.getFromLocationName(addressFromDb,1);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            //Log.e(TAG, "getAddresses: IOException: " + e.getMessage());
-                        }
-
-                        if(addressList.size() > 0) {
-                            Address address = addressList.get(0);
-                            addressLatLng = new LatLng(address.getLatitude(), address.getLongitude());
-                            mMap.addMarker(new MarkerOptions()
-                                .position(addressLatLng)
-                                .title(offerId.child("description").getValue().toString())
-                            );
-                        }
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        */
-    }
-
-    private void enableUserLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-        } else {
-            //Ask for permission
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-                //We need to show user a dialog for displaying why the permission is needed and then ask for the permission...
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        FINE_LOCATION_ACCESS_REQUEST_CODE);
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        FINE_LOCATION_ACCESS_REQUEST_CODE);
-            }
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == FINE_LOCATION_ACCESS_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //We have the permission
-                mMap.setMyLocationEnabled(true);
-            }
-        }
-    }
-
-    private void getCurrentLocation() {
-        //We have the permission
-        Task<Location> locationTask = mFusedLocationClient.getLastLocation();
-        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-            }
-        });
-    }
 }
