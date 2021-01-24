e6389f89f705b0c35599cbf9d370ec4528bb8877
Wojciech <65348511+wojtas112@users.noreply.github.com>
2021-01-24 14:57:40 +0100

    Implementing profile, notifiacation and offers fragments

--- ShareCaring/app/src/main/java/com/example/sharecaring/fragment/NotificationsFragment.java
+++ ShareCaring/app/src/main/java/com/example/sharecaring/fragment/NotificationsFragment.java
@@ -4,18 +4,122 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.sharecaring.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
+
+    ArrayList<String> myOffersId = new ArrayList<>();
+    DatabaseReference ref;
+    FirebaseUser user;
+    FirebaseAuth mAuth;
+    String offerId, userThatAccepted, nameOfUser;
+    LinearLayout layoutList;
+    ArrayList<String> usersThatAccepted = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        View v = inflater.inflate(R.layout.fragment_notifications, container, false);
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        layoutList = v.findViewById(R.id.layout_list);
+        getMyOffersId();
+        getAcceptedOffers();
+        return v;
+    }
+
+    private void getMyOffersId() {
+        String userid = user.getUid();
+        ref = FirebaseDatabase.getInstance().getReference("Offers").child(userid);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    offerId = postSnapshot.getKey();
+                    myOffersId.add(offerId);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
+    }
+
+    private void getAcceptedOffers() {
+        final String userid = user.getUid();
+        ref = FirebaseDatabase.getInstance().getReference("AcceptedOffers");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot userIdDb : snapshot.getChildren()) {
+                    if(!userIdDb.getKey().equals(userid))
+                        for(DataSnapshot acceptedOfferIdDb : userIdDb.getChildren()) {
+                            for (String myOfferId : myOffersId) {
+                                if(acceptedOfferIdDb.getValue().equals(myOfferId)) {
+                                    System.out.println("elooooooss");
+                                    userThatAccepted = userIdDb.getKey();
+                                    System.out.println(userThatAccepted);
+                                    usersThatAccepted.add(userThatAccepted);
+                                    createNotification();
+                                }
+                            }
+                        }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    private void createNotification() {
+        final View myNotificationView = getLayoutInflater().inflate(R.layout.notification, null, false);
+        TextView myNotificationTextView = (TextView)myNotificationView.findViewById(R.id.textViewSingleNotification);
+        //getNameOfUser();
+        System.out.println("name of user outside: " + userThatAccepted);
+        myNotificationTextView.setText(userThatAccepted + " accepted your offer");
+
+        layoutList.addView(myNotificationView);
+
+    }
+
+    private void getNameOfUser() {
+        //userRef = FirebaseDatabase.getInstance().getReference("Users");
+        //userRef.child(userThatAccepted).g
+        ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(userThatAccepted).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                nameOfUser = snapshot.child("firstName").getValue().toString();
+                System.out.println(nameOfUser);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
     }
 }
--- ShareCaring/app/src/main/java/com/example/sharecaring/fragment/ProfileFragment.java
+++ ShareCaring/app/src/main/java/com/example/sharecaring/fragment/ProfileFragment.java
@@ -1,21 +1,130 @@
 package com.example.sharecaring.fragment;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.sharecaring.R;
+import com.example.sharecaring.activity.EditProfileActivity;
+import com.example.sharecaring.activity.MapsActivity;
+import com.example.sharecaring.activity.MyOffersActivity;
+import com.example.sharecaring.activity.NotificationActivity;
+import com.example.sharecaring.activity.OfferList;
+import com.example.sharecaring.activity.ProfileActivity;
+import com.example.sharecaring.activity.StartActivity;
+import com.example.sharecaring.model.IntentOpener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.mikhaellopez.circularimageview.CircularImageView;
+import com.squareup.picasso.Picasso;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener{
+
+    EditText editTextFirstName, editTextLastName, editTextEmail;
+    Button btnMyOffers, btnLogOut, btnEditProfile;
+    DatabaseReference ref;
+    FirebaseUser user;
+    FirebaseAuth mAuth;
+    String firstNameFromDB, lastNameFromDB, emailFromDB;
+    CircularImageView profilePic;
+    StorageReference storageReference;
+    BottomNavigationView bottomNavigationView;
+    FloatingActionButton fabBtn;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //initialize Firebase
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+        editTextFirstName = (EditText)v.findViewById(R.id.editProfileTextFirstName);
+        editTextLastName = (EditText)v.findViewById(R.id.editProfileTextLastName);
+        editTextEmail = (EditText)v.findViewById(R.id.editProfileTextEmail);
+        btnMyOffers = (Button)v.findViewById(R.id.btnFinish);
+        btnMyOffers.setOnClickListener(this);
+        btnLogOut = v.findViewById(R.id.btnLogOut);
+        btnLogOut.setOnClickListener(this);
+        btnEditProfile = v.findViewById(R.id.editBtn);
+        btnEditProfile.setOnClickListener(this);
+        profilePic = v.findViewById(R.id.profileImg);
+        loadData();
+        return v;
+    }
+
+    private void loadData() {
+        String userid = user.getUid();
+        ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(userid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()) {
+                    firstNameFromDB = snapshot.child("firstName").getValue().toString();
+                    lastNameFromDB = snapshot.child("lastName").getValue().toString();
+                    emailFromDB = snapshot.child("email").getValue().toString();
+                }
+                editTextFirstName.setText(firstNameFromDB);
+                editTextLastName.setText(lastNameFromDB);
+                editTextEmail.setText(emailFromDB);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
+        Uri uri = user.getPhotoUrl();
+        if(uri != null) {
+            Picasso.with(getActivity()).load(uri).into(profilePic);
+        }
+
+        String path = userid + "/profilePicture.jpg";
+        final StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        storageReference.child(path).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.with(getActivity()).load(uri).into(profilePic);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btnFinish:
+                IntentOpener.openIntent(getActivity(), MyOffersActivity.class);
+                break;
+            case R.id.btnLogOut:
+                mAuth.signOut();
+                IntentOpener.openIntent(getActivity(), StartActivity.class);
+                //finish();
+                break;
+            case R.id.editBtn:
+                IntentOpener.openIntent(getActivity(), EditProfileActivity.class);
+                break;
+            case R.id.fab:
+                break;
+        }
     }
 }
